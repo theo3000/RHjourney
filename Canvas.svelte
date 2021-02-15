56d9115853090f9a1da7eaa7675ba2f2671ee8bc
theo3000 <51195046+theo3000@users.noreply.github.com>
2021-02-15 10:26:05 +1000

    Create Canvas.svelte

---
+++ Canvas.svelte
@@ -0,0 +1,14 @@
+<script lang='typescript'>
+  import { onMount } from"svelte";
+  import p5 from "p5";
+	export let sketch;
+  let element;
+  onMount(()  => {
+	  let myp5 = new p5(sketch, element);
+	});
+</script>
+
+<style>
+</style>
+
+<div bind:this={element}></div>
